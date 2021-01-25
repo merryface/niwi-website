1f7b05cbe787d7390b41784407eb417ed9f47ed8
Tarik <tarikmerrylees@gmail.com>
2021-01-25 13:31:31 +0000

    created header component

---
+++ src/components/common/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+let Header = () => {
+    return(
+        <header>
+            <div className="header_Title"><a href="/">Night Witches</a></div>
+            {/* <Navbar  /> */}
+        </header>
+    )
+}
+
+export default Header;
