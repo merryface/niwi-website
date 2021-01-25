081517e9ad1afb76abb27767096bca75f22a07a1
Tarik <tarikmerrylees@gmail.com>
2021-01-25 15:18:54 +0000

    header styled

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 let Navbar = () => {
     return(
-        <nav>
-            <p className="nav_Link">Home</p>
-            <p className="nav_Link">Our Squadrons</p>
-            <p className="nav_Link">Videos</p>
-            <p className="nav_Link">Photos</p>
-            <p className="nav_Link">Join us!</p>
+        <nav className="nav_links">
+            <p className="nav_link">Home</p>
+            <p className="nav_link">Our Squadrons</p>
+            <p className="nav_link">Videos</p>
+            <p className="nav_link">Photos</p>
+            <p className="nav_link">Join us!</p>
         </nav>
     )
 }
