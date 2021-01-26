f6ce226d737155a3e45faa3186d499ba599baf84
Tarik <tarikmerrylees@gmail.com>
2021-01-26 15:16:59 +0000

    added background video. autoplay not working

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,6 +7,10 @@ let Hero = () => {
     return (
         <section className="hero">
 
+            <div className="videoContainer">
+                <iframe title="niwi trailer" className="videoContainer__video" width="x" height="y" src="https://www.youtube.com/embed/ipxu81WiQsw?controls=0&amp;start=57?autoplay=1&mute=1" autoplay="1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;"></iframe>
+            </div>
+
             <img src={ niwiLogo } className="hero_logo" alt="Night Witches Logo" />
             <h2 className="hero_subtitle">Who are the Night Withes?</h2>
             <p className="hero_intro">We're a mercenary fighter wing, made up of the best pilots in the galaxy. We hold allegiance to no faction - loyal only to ourselves and whoever happens to hire us. Our motto is <div className="hero_niwiMotto">"Live to Fight and Fly!"</div></p>
