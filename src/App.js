d18d8dab01e2aef7325970c2aa96e7ff427c2245
Tarik <tarikmerrylees@gmail.com>
2021-01-26 09:48:48 +0000

    created hero component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Header, Footer } from './components/common';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Footer />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+let Hero = () => {
+    return (
+        <section className="hero">
+            <img src="/" alt="Night Witches Logo" />
+            <h2>Who are the Night Withes?</h2>
+            <p>We're a mercenary fighter wing, made up of the best pilots in the galaxy. We hold allegiance to no faction - loyal only to ourselves and whoever happens to hire us. Our motto is <div className="hero_niwiMotto">"Live to Fight and Fly!"</div></p>
+            <p>Learn More</p>
+            <img src="/" alt="downards pointing arrow" /> 
+
+        </section>
+    )
+}
+export default Hero;
