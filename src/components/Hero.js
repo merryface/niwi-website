1cc73bf4e18432d5ccc0a8a8b6b377f24a868c2a
Tarik <tarikmerrylees@gmail.com>
2021-01-26 10:02:14 +0000

    added hero image assets

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import downArrow from "../assets/downArrow.svg"
+import niwiLogo from "../assets/niwiLogo.webp"
+
 
 let Hero = () => {
     return (
         <section className="hero">
-            <img src="/" alt="Night Witches Logo" />
+
+            <img src={ niwiLogo } alt="Night Witches Logo" />
             <h2>Who are the Night Withes?</h2>
             <p>We're a mercenary fighter wing, made up of the best pilots in the galaxy. We hold allegiance to no faction - loyal only to ourselves and whoever happens to hire us. Our motto is <div className="hero_niwiMotto">"Live to Fight and Fly!"</div></p>
             <p>Learn More</p>
-            <img src="/" alt="downards pointing arrow" /> 
+            <img src={ downArrow } alt="downards pointing arrow" /> 
 
         </section>
     )
 }
+
 export default Hero;
