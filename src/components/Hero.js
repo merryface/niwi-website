a47d52e06659ce832eadeea83fbadd168e7a1fe1
Tarik <tarikmerrylees@gmail.com>
2021-01-26 14:26:00 +0000

    style: aligned hero elements

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,8 +10,11 @@ let Hero = () => {
             <img src={ niwiLogo } className="hero_logo" alt="Night Witches Logo" />
             <h2 className="hero_subtitle">Who are the Night Withes?</h2>
             <p className="hero_intro">We're a mercenary fighter wing, made up of the best pilots in the galaxy. We hold allegiance to no faction - loyal only to ourselves and whoever happens to hire us. Our motto is <div className="hero_niwiMotto">"Live to Fight and Fly!"</div></p>
-            <p className="hero_learnMore">Learn More</p>
-            <img src={ downArrow } className="hero_downArrow" alt="downards pointing arrow" /> 
+
+            <div className="hero_continue">
+                <p className="hero_learnMore">Learn More</p>
+                <img src={ downArrow } className="hero_downArrow" alt="downards pointing arrow" /> 
+            </div>
 
         </section>
     )
