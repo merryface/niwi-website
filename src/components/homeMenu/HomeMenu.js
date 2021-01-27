754ec975901ec806a11358cc1186c917850e34d7
Tarik <tarikmerrylees@gmail.com>
2021-01-27 14:26:13 +0000

    added menu images and background image

--- src/components/homeMenu/HomeMenu.js
+++ src/components/homeMenu/HomeMenu.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Card from './Card';
+import squadronsImage from '../../assets/squadronsImage.jpg'
+import videoImage from '../../assets/videosImage.jpg'
+import photosImage from '../../assets/photosImage.jpg'
+
 
 let HomeMenu = () => (
     <>
         {/* Squadrons */}
-        <Card image="http://img3.wikia.nocookie.net/__cb20131012080921/starwars/images/8/80/X-wing_Fathead.png" alt="Star Wars squadrons screenshot of new republic pilots" text="See our Squadrons" type="SQUADRONS" />
+        <Card image={ squadronsImage } alt="Star Wars squadrons screenshot of new republic pilots" text="See our Squadrons" type="SQUADRONS" />
 
         {/* Videos */}
-        <Card image="http://img3.wikia.nocookie.net/__cb20131012080921/starwars/images/8/80/X-wing_Fathead.png" alt="" text="Watch Our Videos" type="VIDEOS" />
+        <Card image={ videoImage } alt="Star wars squadrons screenshot of an exploding tie bomber" text="Watch Our Videos" type="VIDEOS" />
 
         {/* Photos */}
-        <Card image="http://img3.wikia.nocookie.net/__cb20131012080921/starwars/images/8/80/X-wing_Fathead.png" alt="" text="See our Photos" type="PHOTOS" />
+        <Card image={ photosImage } alt="star wars squadrons screenshot of tie-defender firing ion lasers" text="See our Photos" type="PHOTOS" />
     </>
     
 )
