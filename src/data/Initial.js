89c1bd984ca2b7a4db19ab2ab7759384ad682e93
Tarik <tarikmerrylees@gmail.com>
2021-01-28 14:57:56 +0000

    added hex info in state

--- src/data/Initial.js
+++ src/data/Initial.js
@@ -1,6 +1,24 @@
 // Initial state
 const initial = {
     page: "home",
+
+    hexInfo: {
+        name: "Hex Squadron",
+        active: true,
+        description: "This is where Hex Squadron's blurb will go",
+        playerRoster: [
+            " \"Scatch\" || Hex Leader",
+            "\"Slacks\" || Hex 2",
+            "\"Professor\" Autumn || Hex 3",
+            "\"Red\" || Hex 4",
+            "\"Wazimbo\" || Hex 5",
+            "\"Clutch\" || Hex 6",
+            "Null || Hex 7",
+            "\"Checkers\" || Hex 10",
+            "\"Swiper\" || Hex 11",
+        ]
+        
+    },
 };
 
 export default initial;
