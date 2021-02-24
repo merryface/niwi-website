775256930016ce1c2d24366d7979a61fede82794
Tarik <tarikmerrylees@gmail.com>
2021-02-24 16:44:11 +0000

    made h1 linkto home

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from '../navbar';
+import { Link } from "react-router-dom";
 
 let Header = () => {
     return(
         <header>
-            <div className="header_Title"><h1>Night Witches</h1></div>
+            <div className="header_Title"><Link to="/"><h1>Night Witches</h1></Link></div>
             <Navbar />
         </header>
     )
