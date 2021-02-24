9bc1cd291ab81c659bd60d855c54b97553cec3b0
Tarik <tarikmerrylees@gmail.com>
2021-02-24 15:14:41 +0000

    added react router to project and added routes to each page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import { Header, Footer } from './components/common';
+import Home from './components/pages/Home';
+import SeeOurSquadrons from './components/pages/SeeOurSquadrons';
+import Videos from './components/pages/Videos';
 import JoinUs from './components/pages/JoinUs';
-
+import { BrowserRouter as Router, Route, } from "react-router-dom";
+import Photos from './components/pages/Photos';
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <JoinUs />
-      <Footer />
+      <Router>
+        <Header />
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/squadrons">
+          <SeeOurSquadrons />
+        </Route>
+
+        <Route exact path="/videos">
+          <Videos />
+        </Route>
+
+        <Route exact path="/photos">
+          <Photos />
+        </Route>
+
+        <Route exact path="/join">
+          <JoinUs />
+        </Route>
+
+        <Footer />
+      </Router>
+
     </div>
   );
 }
