69e0f5ecb8de781bd20da45e480603ec170180f9
Tarik <tarikmerrylees@gmail.com>
2021-02-24 15:21:18 +0000

    added working links to header

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 let Navbar = () => {
     return(
         <nav className="nav_links">
-            <a href="/" className="nav_link">Home</a>
-            <a href="/" className="nav_link">Our Squadrons</a>
-            <a href="/" className="nav_link">Videos</a>
-            <a href="/" className="nav_link">Photos</a>
-            <a href="/" className="nav_link">Join us!</a>
+            <Link to="/" className="nav_link">Home</Link> 
+            <Link to="/squadrons" className="nav_link">Our Squadrons</Link> 
+            <Link to="/videos"  className="nav_link">Videos</Link> 
+            <Link to="/photos"  className="nav_link">Photos</Link> 
+            <Link to="/join"  className="nav_link">Join us!</Link> 
         </nav>
     )
 }
