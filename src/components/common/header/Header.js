e53857ddaec86db9c814c32038b214cfa57c8f02
Tarik <tarikmerrylees@gmail.com>
2021-02-24 16:54:36 +0000

    fixed h1 link styling

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 let Header = () => {
     return(
         <header>
-            <div className="header_Title"><Link to="/"><h1>Night Witches</h1></Link></div>
+            <Link className="header_Title" to="/"><h1 className="header_Title">Night Witches</h1></Link>
             <Navbar />
         </header>
     )
