c2de20047b1af8033a81324118cc8938ecac413f
Tarik <tarikmerrylees@gmail.com>
2021-02-25 16:17:43 +0000

    added feature remove menu when item clicked

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 let Menu = () => {
+    const dispatch = useDispatch();
+
+    // Burger toggle
+    let menuToggle = () => dispatch({
+        type: "MENU_DISPLAYED"
+    });
+
     // Add menu state
     const menuDisplayed = useSelector(state => state.menuDisplayed);
 
@@ -11,11 +19,11 @@ let Menu = () => {
         return (
             <>
             <nav className="menu_links">
-                    <Link to="/" className="menu_link">Home</Link> 
-                    <Link to="/squadrons" className="menu_link">Our Squadrons</Link> 
-                    <Link to="/videos"  className="menu_link">Videos</Link> 
-                    <Link to="/photos"  className="menu_link">Photos</Link> 
-                    <Link to="/join"  className="menu_link">Join us!</Link> 
+                    <Link to="/" className="menu_link" onClick={ menuToggle }>Home</Link> 
+                    <Link to="/squadrons" className="menu_link" onClick={ menuToggle }>Our Squadrons</Link> 
+                    <Link to="/videos"  className="menu_link" onClick={ menuToggle }>Videos</Link> 
+                    <Link to="/photos"  className="menu_link" onClick={ menuToggle }>Photos</Link> 
+                    <Link to="/join"  className="menu_link" onClick={ menuToggle }>Join us!</Link> 
             </nav>
         </>
         )
