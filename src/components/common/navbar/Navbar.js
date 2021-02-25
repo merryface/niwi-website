68ec74895ce8402f89632b49c446fe1ff64a8688
Tarik <tarikmerrylees@gmail.com>
2021-02-25 14:21:02 +0000

    added burger menu icon

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import niwiLogo from "../../../assets/niwiLogo.webp"
 
 let Navbar = () => {
     return(
+        <>
         <nav className="nav_links">
             <Link to="/" className="nav_link">Home</Link> 
             <Link to="/squadrons" className="nav_link">Our Squadrons</Link> 
@@ -10,6 +12,8 @@ let Navbar = () => {
             <Link to="/photos"  className="nav_link">Photos</Link> 
             <Link to="/join"  className="nav_link">Join us!</Link> 
         </nav>
+        <img className="header_menuIcon" src={ niwiLogo } />
+        </>
     )
 }
 
